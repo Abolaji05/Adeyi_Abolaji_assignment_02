82e913c9d1b2b9b40f215e4bbc6a0a6f28fbe4a6
Abolaji Adeyi <102436106+Abolaji05@users.noreply.github.com>
2022-07-14 19:54:54 +0000

    challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -1,4 +1,4 @@
-// ========================
+ // ========================
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
 // ========================
@@ -14,9 +14,10 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length;
+  return arrayOfNumbers.reduce((a,b) => a+b,0);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +29,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length;
+  const even = arrayOfNumbers.filter(number => {
+  return number % 2 === 0;
+});
+
+return(even.length); 
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
